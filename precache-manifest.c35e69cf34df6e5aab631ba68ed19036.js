4d430522df4dddcd0d58951c6d80084ae6dd2ef6
Vinicios Macario <JackWyus_01@outlook.com>
2022-12-12 18:23:06 -0300

    Updates

---
+++ precache-manifest.c35e69cf34df6e5aab631ba68ed19036.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77212c1a2e6a7d939c217165f71659a0",
+    "url": "/Smart-Bank/index.html"
+  },
+  {
+    "revision": "c9dbb149ff47b2190dea",
+    "url": "/Smart-Bank/static/js/2.1ccf19f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Smart-Bank/static/js/2.1ccf19f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c45180309c5d35c235",
+    "url": "/Smart-Bank/static/js/main.ae322b03.chunk.js"
+  },
+  {
+    "revision": "f5fdaca1396b4d394cf4",
+    "url": "/Smart-Bank/static/js/runtime-main.fb9d8601.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/Smart-Bank/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "3ebcea6560ce5427d3c58b7cc866251a",
+    "url": "/Smart-Bank/static/media/bank_logo.3ebcea65.svg"
+  },
+  {
+    "revision": "30bb1f760cde86d36c2a7100b7acf824",
+    "url": "/Smart-Bank/static/media/dinheiro.30bb1f76.svg"
+  },
+  {
+    "revision": "a835324f97732136c12bfcf434407a88",
+    "url": "/Smart-Bank/static/media/olho.a835324f.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/Smart-Bank/static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "8191b12a4ac16e0b7bb63c48bb5433b0",
+    "url": "/Smart-Bank/static/media/privado.8191b12a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/Smart-Bank/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "664a5b4d841c2319951a6057fda7c50d",
+    "url": "/Smart-Bank/static/media/themeOff.664a5b4d.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/Smart-Bank/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/Smart-Bank/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "cd27a55086bec350b880613e525e215b",
+    "url": "/Smart-Bank/static/media/utilidades.cd27a550.svg"
+  }
+]);
